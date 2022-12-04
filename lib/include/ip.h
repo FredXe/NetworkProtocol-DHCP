405092b7b21e3504b3f528bb1b074350b93d0585
FredXe <freddy115910810@gmail.com>
2022-12-05 07:23:31 +0800

    comment ip.h netdevice.h

--- lib/include/ip.h
+++ lib/include/ip.h
@@ -11,8 +11,9 @@
 #define IP_ALLOC(ip) u_int8_t *ip = (u_int8_t *)calloc(IP_ADDR_LEN, sizeof(u_int8_t))
 
 /**
- *	Convert string into byte array
- *	@param ip_addr_str *.*.*.* format string
+ * Convert string into byte array
+ * @param ip_addr_str *.*.*.* format string
+ * @return u_int8_t* point to ip_addr
  */
 u_int8_t *string_to_ip_addr(char *ip_addr_str);
 
--- lib/include/netdevice.h
+++ lib/include/netdevice.h
@@ -8,9 +8,13 @@
 #define ETH_ALLOC(addr) u_int8_t *addr = (u_int8_t *)calloc(ETH_ADDR_LEN, sizeof(u_int8_t))
 
 /**
- *	Convert string into byte array
- *	@param eth_addr_str **:**:**:**:**:** format string
+ * @brief Convert string into byte array
+ *
+ * @param eth_addr_str **:**:**:**:**:** format string
+ * @return u_int8_t* point to eth_addr
  */
 u_int8_t *string_to_eth_addr(char *eth_addr_str);
+
+int netdevice_getdevice();
 
 #endif
