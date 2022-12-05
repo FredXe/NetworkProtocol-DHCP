e23d848ec204aaf6fc47a6a25c5fe2c3075a5b26
FredXe <freddy115910810@gmail.com>
2022-12-05 11:39:15 +0800

    comment netdevice_getdevice() @ netdevice.h

--- lib/include/netdevice.h
+++ lib/include/netdevice.h
@@ -7,18 +7,26 @@
 #define ETH_ADDR_LEN	6
 
 /**
- *	Script for allocate an eth address
+ * Script for allocate an eth address
  */
 #define ETH_ALLOC(addr) u_int8_t *addr = (u_int8_t *)calloc(ETH_ADDR_LEN, sizeof(u_int8_t))
 
+/**
+ * Get device from pcap_findalldevs(),
+ * than select one of it.
+ * @param dev_sel_no '0' - select from terminal.
+ * 'non0' - select directly from parameter
+ * @param dev_name
+ * @return 0 if success.
+ * NETDEVICE_ERROR if error
+ */
 int netdevice_getdevice(const int dev_sel_no, char *dev_name);
 
 /**
  * Convert string into byte array
- *
  * @param eth_addr_str **:**:**:**:**:** format string
- * @return u_int8_t* point to eth_addr,
- * 	NULL if error
+ * @return u_int8_t* point to eth_addr.
+ * NULL if error
  */
 u_int8_t *string_to_eth_addr(char *eth_addr_str);
 
