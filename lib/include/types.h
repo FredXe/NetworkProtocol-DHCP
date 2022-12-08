03f0b376b78f13c7e0859cd6b4181a2c22c75e6f
FredXe <freddy115910810@gmail.com>
2022-12-09 00:15:23 +0800

    docs(types.h): comment the types

--- lib/include/types.h
+++ lib/include/types.h
@@ -3,8 +3,8 @@
 
 #include <sys/types.h>
 
-typedef u_int8_t byte;
-typedef u_int16_t two_bytes;
-typedef u_int32_t ip_addr_t;
+typedef u_int8_t byte;		   // One byte
+typedef u_int16_t two_bytes;   // Two bytes
+typedef u_int32_t ip_addr_t;   // Four bytes (IPv4 address)
 
 #endif
