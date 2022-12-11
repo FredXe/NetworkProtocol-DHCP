7207a066c9207fed741acc8ebe8f146ab55d01ae
FredXe <freddy115910810@gmail.com>
2022-12-11 17:31:06 +0800

    feat(lib/include/ip.h): define IPv4 header format

--- lib/include/ip.h
+++ lib/include/ip.h
@@ -17,6 +17,38 @@
  */
 #define IP_ALLOC(ip) byte *ip = (byte *)calloc(IP_ADDR_LEN, sizeof(byte))
 
+/*=================
+ * Protocol Format
+ *=================*/
+typedef struct {
+	// [1-4] Version. [5-8] Header length.
+	byte ver_hlen;
+	// [1-6] Differentiated Services. [7-8] Explicit Congestion Notification.
+	byte DS_ECN;
+	// Total length
+	two_bytes length;
+
+	// Identification
+	two_bytes id;
+	// [1-3] Flags. [4-16] Fragment Offset.
+	two_bytes falg_frgofst;
+	// Time to live
+	byte ttl;
+	// Protocol of upper layer
+	byte protocol;
+	// Header Checksum
+	two_bytes hdr_chksum;
+
+	// Source IP address
+	ip_addr_t src_ip;
+	// Destination IP address
+	ip_addr_t dst_ip;
+} ipv4_hdr_t;	// IPv4 header format
+
+// Script to read version & hlen in ver_hlen
+#define VER(ip_hdr)	 ((ip_hdr)->ver_hlen >> 4)
+#define HLEN(ip_hdr) ((ip_hdr)->ver_hlen & 0x0F)
+
 /*================
  * Public Methods
  *================*/
